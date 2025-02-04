5e283a5118364265ccb9e4d55ac76ea4a1912871
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-02-04 22:04:19 +0200

    Intro to Dashboard

---
+++ E-Commerce510/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce510.Data;
+using E_Commerce510.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce510.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            var categories = dbContext.Categories;
+            //
+            return View(categories.ToList());
+        }
+
+        public IActionResult CreateNew()
+        {
+            return View();
+        }
+
+        public IActionResult SaveNew(string categoryName)
+        {
+            if(categoryName != null)
+            {
+                dbContext.Categories.Add(new Category()
+                {
+                    Name = categoryName
+                });
+                dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+    }
+}
---
+++ E-Commerce510/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce510.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
